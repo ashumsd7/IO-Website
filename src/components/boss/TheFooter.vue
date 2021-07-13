b31b0fb7d8a14d3760b02fd9744f1c99890ed7fc
Aashutosh Anand Tiwari <40313523+ashumsd7@users.noreply.github.com>
2021-07-14 02:24:20 +0530

    updated
    moved to bootstrap

---
+++ src/components/boss/TheFooter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="conatiner">
+ 
+            <div class=" d-flex justify-content-center align-items-center footer text-center bg-warning text-dark">Copyright © IO EXTENDED 2021. All rights reserved. </div>
+      
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style  scoped>
+.footer{
+    font-weight: bolder;
+    height: 40px;
+    font-size: .9rem;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,249 @@
+<template>
+  <div>
+    <div class="container-fluid">
+      <div class="row">
+        <div
+          class="col main-header bg-warning text-dark font-weight-bold d-flex flex-column justify-content-center align-items-center"
+          style="height: 370px"
+        >
+        <h1>I/O</h1>
+       <h2 id="tag2">Experience IO</h2> 
+       <h2 id="tag3">With your local communties</h2>
+       <button class="bg-light rounded p-2 outline-none border-0">Register Now</button>
+        
+        </div>
+      </div>
+    </div>
+
+<!-- ______________WHY IO EXTENDED___________START -->
+
+
+
+    <div class="container">
+      <div class="row my-1 m-auto">
+        <div class="col my-3">
+          <h1>Why <span>IO Extended ?</span></h1>
+        </div>
+      </div>
+      <div class="row my-1 d-flex justify-content-around">
+        <div class="col-md-5 m-2 p-3 community-div rounded">
+          <h3 class="mb-3">Because Community matters</h3>
+          <p>
+            Amet sit sadipscing ea labore ea duo stet, magna diam labore, lorem
+            aliquyam accusam vero no duo invidunt dolor. Sed sanctus erat diam
+            lorem est consetetur et gubergren, no sed elitr lorem ipsum.
+            Sadipscing lorem nonumy vero sit kasd tempor sanctus sit ipsum, et
+            sit no consetetur no erat stet, diam tempor ut sed magna takimata
+            takimata voluptua takimata. Sit eirmod duo amet lorem. Sit magna est
+            kasd sanctus nonumy amet dolore takimata,.
+          </p>
+        </div>
+        <div class="col-md-5 m-2 p-2 growth-div rounded">
+          <h3 class="mb-3">Because Community matters </h3>
+          <p>
+            Amet sit sadipscing ea labore ea duo stet, magna diam labore, lorem
+            aliquyam accusam vero no duo invidunt dolor. Sed sanctus erat diam
+            lorem est consetetur et gubergren, no sed elitr lorem ipsum.
+            Sadipscing lorem nonumy vero sit kasd tempor sanctus sit ipsum, et
+            sit no consetetur no erat stet, diam tempor ut sed magna takimata
+            takimata voluptua takimata. Sit eirmod duo amet lorem. Sit magna est
+            kasd sanctus nonumy amet dolore takimata,
+          </p>
+        </div>
+      </div>
+
+      <div class="row my-2 d-flex justify-content-around">
+        <div class="col-md-8 m-2 p-3 you-matter-div rounded">
+          <h3>Because YOU matter</h3>
+          <p>
+            Amet sit sadipscing ea labore ea duo stet, ea nonumy et sed amet
+            consetetur magna diam labore, lorem aliquyam accusam vero no duo
+            invidunt dolor. Sed sanctus erat diam lorem est consetetur et
+            gubergren, no sed elitr lorem ipsum. Sadipscing lorem nonumy vero
+            sit kasd tempor sanctus sit ipsum, et sit no consetetur no erat
+            stet, diam tempor ut sed magna takimata takimata voluptua takimata.
+            Sit eirmod duo amet lorem. Sit magna est kasd sanctus nonumy amet
+            dolore takimata,
+          </p>
+        </div>
+        <div class="col-md-2 m-1 p-3 calender-div text-center rounded">
+          <img src="../assets/calendar.png" alt="" />
+          <h2 class="mt-1">16 July</h2>
+          <h3>Friday</h3>
+        </div>
+      </div>
+    </div>
+
+    <!-- ______________WHY IO EXTENDED___________END -->
+
+    <!-- ______________CELEBRATION OF TECH___________START -->
+
+    <div class="container mt-5">
+      <div class="row mt-5">
+        <div class="col m-auto d-flex justify-content-center">
+          <h3 class="heading">
+            A Local <span id="IO">Celebration of Tech </span>
+          </h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col m-auto d-flex justify-content-center">
+          <img src="../assets/tech.png" alt="" class="img-fluid" />
+        </div>
+      </div>
+    </div>
+    <!-- ______________CELEBRATION OF _________END -->
+
+    <!-- ______________SPEAKERS LINEUP___________START -->
+
+    <div class="container mt-5">
+      <div class="row mt-5">
+        <div class="col m-auto d-flex justify-content-center">
+          <h3 class="heading mb-5">Speakers <span id="IO">Lineup </span></h3>
+        </div>
+      </div>
+      <div class="row m-auto d-flex justify-content-center">
+        <div class="col-lg-10">
+          <div class="row">
+            <div class="col-md-5 text-center">
+              <div class="row">
+                <h4>Jane Cooper</h4>
+                <p>CEO at ABC Company</p>
+              </div>
+
+              <div class="row">
+                <h4>Jane Cooper</h4>
+                <p>CEO at ABC Company</p>
+              </div>
+
+              <div class="row">
+                <h4>Jane Cooper</h4>
+                <p>CEO at ABC Company</p>
+              </div>
+            </div>
+            <div class="col-md-5 text-center">
+              <div class="row">
+                <h4>Jane Cooper</h4>
+                <p>CEO at ABC Company</p>
+              </div>
+
+              <div class="row">
+                <h4>Jane Cooper</h4>
+                <p>CEO at ABC Company</p>
+              </div>
+
+              <div class="row">
+                <h4>Jane Cooper</h4>
+                <p>CEO at ABC Company</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- ______________SPEAKERS LINEUP___________END -->
+
+     <!-- ______________PARTNERS COMMUNITIES__________START -->
+
+         <div class="container mt-5">
+      <div class="row mt-5">
+        <div class="col m-auto d-flex justify-content-center">
+          <h3 class="heading">
+            Partner <span id="IO">Communities </span>
+          </h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col m-auto d-flex justify-content-center">
+          <img src="../assets/partners.png" alt="" class="img-fluid">
+        </div>
+      </div>
+    </div>
+
+ <!-- ______________PARTNERS COMMUNITIES__________END -->
+
+
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style  scoped>
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+
+
+
+/* _________MAIN_HEADER_START___________ */
+.main-header h1{
+    /* font-weight: bolder; */
+    font-family: 'Bebas Neue', cursive;
+    color: rgb(0, 0, 0);
+    font-weight: bolder;
+    font-size: 5rem;
+    font-family: 'Ultra', serif;
+}
+.main-header #tag1{
+    color: rgb(0, 0, 0);
+    letter-spacing: 2px;
+    font-family: 'Noto Sans JP', sans-serif;
+}
+.main-header #tag2{
+color: black;
+    word-spacing: 3px;
+}
+/* _________MAIN_HEADER_END___________ */
+
+/* __________MATTERS DIV START_________ */
+.col {
+  font-family: "Open Sans", sans-serif;
+}
+h1 span {
+  font-weight: bolder;
+}
+
+.community-div {
+  background: rgb(250, 140, 136);
+}
+.community-div,
+.growth-div,
+.you-matter-div,
+.calender-div {
+   /* border-radius: 20px;  */
+box-shadow: 10px 14px 17px -8px rgba(0,0,0,0.66);
+-webkit-box-shadow: 10px 14px 17px -8px rgba(0,0,0,0.66);
+-moz-box-shadow: 10px 14px 17px -8px rgba(0,0,0,0.66);
+}
+
+.community-div p,
+.growth-div p {
+  margin: 10px;
+  font-weight: 600;
+  font-family: "Roboto", sans-serif;
+}
+.growth-div {
+  background: rgb(112, 236, 133);
+}
+.you-matter-div {
+  background: rgb(217, 255, 0);
+}
+.calender-div {
+  background: rgb(67, 147, 252);
+}
+
+/* __________MATTERS DIV END */
+
+/* ______CELEBRATION OF TECH START________ */
+
+#IO {
+  font-weight: 900;
+}
+.heading {
+  font-weight: bolder;
+  font-size: 2rem;
+}
+/* ______CELEBRATION OF TECH END */
+</style>
