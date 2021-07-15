d1783ed37d9beb52335a62c029ac42b21f614827
Aashutosh Anand Tiwari <40313523+ashumsd7@users.noreply.github.com>
2021-07-15 14:34:22 +0530

    updated
    Event and some update are left

--- src/components/boss/TheFooter.vue
+++ src/components/boss/TheFooter.vue
@@ -1,21 +1,103 @@
 <template>
-    <div class="conatiner">
- 
-            <div class=" d-flex justify-content-center align-items-center footer text-center bg-warning text-dark">Copyright © IO EXTENDED 2021. All rights reserved. </div>
-      
+  <div class="conatiner">
+    <div class="footer  bg-warning text-dark">
+      <div class="row d-flex flex-column">
+        <div class="col-lg">
+          <div class="row">
+            <div id="logo" class="col-3 mx-4 my-2"></div>
+            <div id="logo-slash"></div>
+            <div id="logo2" class="col-3"></div>
+          </div>
+        </div>
+        <div class="col-lg ml-3">
+            <div id="logo-name" class="col-6"> <span>Extended</span> Universal</div>
+        </div>
+        <div class="col-lg">
+             <button class="bg-light register-btn p-2  outline-none border-0">
+            Register Now
+          </button>
+        </div>
+      </div>
     </div>
+    <div class="row d-lg-block design-banner-circle"></div>
+    <div class="row d-lg-block design-banner-square"></div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style  scoped>
-.footer{
-    font-weight: bolder;
-    height: 40px;
-    font-size: .9rem;
+.conatiner {
+  position: relative;
+  overflow: hidden;
+}
+.footer {
+  /* font-weight: bolder; */
+  height: 180px;
+  font-size: 0.9rem;
+}
+.design-banner-circle {
+  width: 200px;
+  height: 200px;
+  /* background: black; */
+  border: 2px solid black;
+
+  border-radius: 50%;
+  position: absolute;
+  top: -60%;
+  right: -10%;
+}
+.design-banner-square {
+  width: 200px;
+  height: 200px;
+  /* background: black; */
+  border: 2px solid black;
+
+  position: absolute;
+  bottom: -170px;
+  left: -20px;
+}
+/* _for I rectangle / */
+#logo {
+  width: 20px;
+  height: 50px;
+  background: black;
+  margin-left: -10px;
+}
+/* _for / */
+#logo-slash {
+  width: 1px;
+  height: 60px;
+  background: black;
+  transform: rotateY(84deg) rotateX(-20deg);
+  transform-origin: left;
+  margin-left: -10px;
+  margin-top: 5px;
+}
+/* _for O rounded O */
+#logo2 {
+  width: 50px;
+  height: 50px;
+  background: black;
+  border-radius: 50%;
+  margin-left: -10px;
+  margin-top: 10px;
+}
+
+#logo-name{
+  font-family: "Poppins", sans-serif;
+   margin-left: 10px;
+ 
+  
+}
+#logo-name span{
+  font-weight: bolder;
+  
+}
+.register-btn {
+  border-radius: 10px;
+  margin-left: 10px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,86 @@
 <template>
   <div>
-    <div class="container-fluid">
+    <div class="container-fluid content-banner">
       <div class="row">
         <div
-          class="col main-header bg-warning text-dark font-weight-bold d-flex flex-column justify-content-center align-items-center"
+          class="
+            col
+            main-header
+            bg-warning
+            text-dark
+            font-weight-bold
+            d-flex
+            flex-column
+            justify-content-center
+            align-items-center
+          "
           style="height: 370px"
         >
-        <h1>I/O</h1>
-       <h2 id="tag2">Experience IO</h2> 
-       <h2 id="tag3">With your local communties</h2>
-       <button class="bg-light rounded p-2 outline-none border-0">Register Now</button>
-        
+          <!-- <h1>I/O</h1> -->
+          <div class="row">
+            <div id="logo" class="col-6 mx-4 my-2"></div>
+            <div id="logo-slash"></div>
+            <div id="logo2" class="col-6"></div>
+          </div>
+          <h2 id="tag2">Experience IO</h2>
+          <h2 id="tag3">With your local communties</h2>
+          <button class="bg-light register-btn p-2 mt-3 outline-none border-0">
+            Register Now
+          </button>
         </div>
+      </div>
+      <div class="row  d-lg-block  design-banner-square" >
+
+      </div>
+        <div class="row  d-lg-block  design-banner-circle" >
+
       </div>
     </div>
 
-<!-- ______________WHY IO EXTENDED___________START -->
-
-
+    <!-- ______________WHY IO EXTENDED___________START -->
 
     <div class="container">
       <div class="row my-1 m-auto">
-        <div class="col my-3">
+        <div class="col my-3 box">
           <h1>Why <span>IO Extended ?</span></h1>
         </div>
       </div>
       <div class="row my-1 d-flex justify-content-around">
-        <div class="col-md-5 m-2 p-3 community-div rounded">
-          <h3 class="mb-3">Because Community matters</h3>
-          <p>
-            Amet sit sadipscing ea labore ea duo stet, magna diam labore, lorem
-            aliquyam accusam vero no duo invidunt dolor. Sed sanctus erat diam
-            lorem est consetetur et gubergren, no sed elitr lorem ipsum.
-            Sadipscing lorem nonumy vero sit kasd tempor sanctus sit ipsum, et
-            sit no consetetur no erat stet, diam tempor ut sed magna takimata
-            takimata voluptua takimata. Sit eirmod duo amet lorem. Sit magna est
-            kasd sanctus nonumy amet dolore takimata,.
-          </p>
+        <div class="col-md-5 m-2 p-3 community-div">
+          <h2 class="mb-3 mt-2">Because Community matters</h2>
+          <div class="row">
+            <div class="col-lg-10">
+              <p>
+                Amet sit sadipscing ea labore ea duo stet, magna diam labore,
+                lorem aliquyam accusam vero no duo invidunt dolor. Sed sanctus
+                erat diam lorem est consetetur et gubergren, no sed elitr lorem
+                ipsum. Sadipscing lorem nonumy vero sit kasd tempor sanctus sit
+                ipsum, et sit no consetetur no erat stet, diam tempor ut sed
+                magna takimata
+              </p>
+            </div>
+          </div>
         </div>
-        <div class="col-md-5 m-2 p-2 growth-div rounded">
-          <h3 class="mb-3">Because Community matters </h3>
-          <p>
-            Amet sit sadipscing ea labore ea duo stet, magna diam labore, lorem
-            aliquyam accusam vero no duo invidunt dolor. Sed sanctus erat diam
-            lorem est consetetur et gubergren, no sed elitr lorem ipsum.
-            Sadipscing lorem nonumy vero sit kasd tempor sanctus sit ipsum, et
-            sit no consetetur no erat stet, diam tempor ut sed magna takimata
-            takimata voluptua takimata. Sit eirmod duo amet lorem. Sit magna est
-            kasd sanctus nonumy amet dolore takimata,
-          </p>
+        <div class="col-md-5 m-2 p-2 growth-div">
+          <h2 class="mb-3 mt-2">Because Community matters</h2>
+          <div class="row">
+            <div class="col-lg-10">
+              <p>
+                Amet sit sadipscing ea labore ea duo stet, magna diam labore,
+                lorem aliquyam accusam vero no duo invidunt dolor. Sed sanctus
+                erat diam lorem est consetetur et gubergren, no sed elitr lorem
+                ipsum. Sadipscing lorem nonumy vero sit kasd tempor sanctus sit
+                ipsum, et sit no consetetur no erat stet, diam tempor ut sed
+                magna takimata
+              </p>
+            </div>
+          </div>
         </div>
       </div>
 
       <div class="row my-2 d-flex justify-content-around">
-        <div class="col-md-8 m-2 p-3 you-matter-div rounded">
-          <h3>Because YOU matter</h3>
+        <div class="col-md-8 m-2 p-3 you-matter-div">
+          <h2 class="mt-2">Because YOU matter</h2>
           <p>
             Amet sit sadipscing ea labore ea duo stet, ea nonumy et sed amet
             consetetur magna diam labore, lorem aliquyam accusam vero no duo
@@ -66,7 +92,7 @@
             dolore takimata,
           </p>
         </div>
-        <div class="col-md-2 m-1 p-3 calender-div text-center rounded">
+        <div class="col-md-2 m-1 p-3 calender-div text-center">
           <img src="../assets/calendar.png" alt="" />
           <h2 class="mt-1">16 July</h2>
           <h3>Friday</h3>
@@ -80,10 +106,10 @@
 
     <div class="container mt-5">
       <div class="row mt-5">
-        <div class="col m-auto d-flex justify-content-center">
-          <h3 class="heading">
+        <div class="col m-auto mb-4 box d-flex justify-content-center">
+          <h1 class="heading">
             A Local <span id="IO">Celebration of Tech </span>
-          </h3>
+          </h1>
         </div>
       </div>
       <div class="row">
@@ -98,43 +124,103 @@
 
     <div class="container mt-5">
       <div class="row mt-5">
-        <div class="col m-auto d-flex justify-content-center">
-          <h3 class="heading mb-5">Speakers <span id="IO">Lineup </span></h3>
+        <div class="col m-auto box d-flex justify-content-center">
+          <h1 class="mb-5">Speakers <span id="IO">Lineup </span></h1>
         </div>
       </div>
-      <div class="row m-auto d-flex justify-content-center">
-        <div class="col-lg-10">
-          <div class="row">
+      <div class="row">
+        <div class="col-lg">
+          <div class="row d-flex justify-content-center align-items-center">
             <div class="col-md-5 text-center">
-              <div class="row">
-                <h4>Jane Cooper</h4>
-                <p>CEO at ABC Company</p>
+              <div class="row mb-3">
+                <div class="col-lg-4 ceo">
+                  <img
+                    width="100"
+                    height="100"
+                    src="../assets/ceo.png"
+                    alt=""
+                  />
+                </div>
+                <div class="col-lg-7 d-flex flex-column justify-content-center">
+                  <h4>Jane Cooper</h4>
+                  <p>CEO at ABC Company</p>
+                </div>
               </div>
 
-              <div class="row">
-                <h4>Jane Cooper</h4>
-                <p>CEO at ABC Company</p>
+              <div class="row mb-3">
+                <div class="col-lg-4 ceo">
+                  <img
+                    width="100"
+                    height="100"
+                    src="../assets/ceo.png"
+                    alt=""
+                  />
+                </div>
+                <div class="col-lg-7 d-flex flex-column justify-content-center">
+                  <h4>Jane Cooper</h4>
+                  <p>CEO at ABC Company</p>
+                </div>
               </div>
 
-              <div class="row">
-                <h4>Jane Cooper</h4>
-                <p>CEO at ABC Company</p>
+              <div class="row mb-3">
+                <div class="col-lg-4 ceo">
+                  <img
+                    width="100"
+                    height="100"
+                    src="../assets/ceo.png"
+                    alt=""
+                  />
+                </div>
+                <div class="col-lg-7 d-flex flex-column justify-content-center">
+                  <h4>Jane Cooper</h4>
+                  <p>CEO at ABC Company</p>
+                </div>
               </div>
             </div>
             <div class="col-md-5 text-center">
-              <div class="row">
-                <h4>Jane Cooper</h4>
-                <p>CEO at ABC Company</p>
+              <div class="row mb-3">
+                <div class="col-lg-4 ceo">
+                  <img
+                    width="100"
+                    height="100"
+                    src="../assets/ceo.png"
+                    alt=""
+                  />
+                </div>
+                <div class="col-lg-7 d-flex flex-column justify-content-center">
+                  <h4>Jane Cooper</h4>
+                  <p>CEO at ABC Company</p>
+                </div>
               </div>
 
-              <div class="row">
-                <h4>Jane Cooper</h4>
-                <p>CEO at ABC Company</p>
+              <div class="row mb-3">
+                <div class="col-lg-4 ceo">
+                  <img
+                    width="100"
+                    height="100"
+                    src="../assets/ceo.png"
+                    alt=""
+                  />
+                </div>
+                <div class="col-lg-7 d-flex flex-column justify-content-center">
+                  <h4>Jane Cooper</h4>
+                  <p>CEO at ABC Company</p>
+                </div>
               </div>
 
-              <div class="row">
-                <h4>Jane Cooper</h4>
-                <p>CEO at ABC Company</p>
+              <div class="row mb-3">
+                <div class="col-lg-4 ceo">
+                  <img
+                    width="100"
+                    height="100"
+                    src="../assets/ceo.png"
+                    alt=""
+                  />
+                </div>
+                <div class="col-lg-7 d-flex flex-column justify-content-center">
+                  <h4>Jane Cooper</h4>
+                  <p>CEO at ABC Company</p>
+                </div>
               </div>
             </div>
           </div>
@@ -144,26 +230,24 @@
 
     <!-- ______________SPEAKERS LINEUP___________END -->
 
-     <!-- ______________PARTNERS COMMUNITIES__________START -->
+    <!-- ______________PARTNERS COMMUNITIES__________START -->
 
-         <div class="container mt-5">
+    <div class="container mt-5">
       <div class="row mt-5">
-        <div class="col m-auto d-flex justify-content-center">
-          <h3 class="heading">
+        <div class="col box m-auto d-flex justify-content-center">
+          <h1 class="heading mb-4">
             Partner <span id="IO">Communities </span>
-          </h3>
+          </h1>
         </div>
       </div>
       <div class="row">
         <div class="col m-auto d-flex justify-content-center">
-          <img src="../assets/partners.png" alt="" class="img-fluid">
+          <img src="../assets/partners.png" alt="" class="img-fluid" />
         </div>
       </div>
     </div>
 
- <!-- ______________PARTNERS COMMUNITIES__________END -->
-
-
+    <!-- ______________PARTNERS COMMUNITIES__________END -->
   </div>
 </template>
 
@@ -173,27 +257,96 @@ export default {};
 <style  scoped>
 @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
-@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
-
-
+@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap");
+/* @import url('https://fonts.googleapis.com/css2?family=Coda+Caption:wght@800&display=swap'); */
+@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;700&display=swap");
 
 /* _________MAIN_HEADER_START___________ */
-.main-header h1{
-    /* font-weight: bolder; */
-    font-family: 'Bebas Neue', cursive;
-    color: rgb(0, 0, 0);
-    font-weight: bolder;
-    font-size: 5rem;
-    font-family: 'Ultra', serif;
+/* .main-header h1 { */
+/* font-weight: bolder; */
+/* font-family: "Bebas Neue", cursive;
+  color: rgb(0, 0, 0);
+  font-weight: bolder;
+  font-size: 5rem; */
+/* font-family: 'Ultra', serif; */
+/* font-family: 'Coda Caption', sans-serif; */
+/* font-family: "Poppins", sans-serif;
+} */
+
+#logo {
+  width: 40px;
+  height: 90px;
+  background: black;
 }
-.main-header #tag1{
-    color: rgb(0, 0, 0);
-    letter-spacing: 2px;
-    font-family: 'Noto Sans JP', sans-serif;
+#logo-slash {
+  width: 1px;
+  height: 140px;
+  background: black;
+  transform: rotateY(84deg) rotateX(-20deg);
+  transform-origin: left;
+  margin-top: -10px;
 }
-.main-header #tag2{
-color: black;
-    word-spacing: 3px;
+
+#logo2 {
+  width: 100px;
+  height: 100px;
+  background: black;
+  border-radius: 50%;
+}
+.content-banner{
+  position: relative;
+  overflow: hidden;
+}
+.design-banner-square{
+  width: 200px;
+  height: 200px;
+  /* background: black; */
+  border: 2px solid black;
+  
+
+  position: absolute;
+  bottom: -20px;
+  left: -20px;
+  
+}
+.design-banner-circle{
+    width: 300px;
+  height: 300px;
+  /* background: black; */
+  border: 2px solid black;
+  
+  border-radius: 50%;
+  position: absolute;
+  top: -40%;
+  right: -10%;
+}
+
+
+@media screen and (max-width:750px) {
+  .design-banner-square,.design-banner-circle{
+    display: none;
+  }
+}
+
+.main-header #tag1 {
+  color: rgb(0, 0, 0);
+  letter-spacing: 2px;
+  font-family: "Noto Sans JP", sans-serif;
+}
+.register-btn {
+  border-radius: 10px;
+}
+.main-header #tag2 {
+  color: black;
+  word-spacing: 3px;
+  font-family: "Poppins", sans-serif;
+  font-weight: bolder;
+  font-size: 2.5rem;
+}
+.main-header #tag3 {
+  font-family: "Poppins", sans-serif;
 }
 /* _________MAIN_HEADER_END___________ */
 
@@ -205,17 +358,21 @@ h1 span {
   font-weight: bolder;
 }
 
+.box h1 {
+  font-family: "Poppins", sans-serif;
+}
 .community-div {
-  background: rgb(250, 140, 136);
+  background: #f88c84;
 }
 .community-div,
 .growth-div,
 .you-matter-div,
 .calender-div {
-   /* border-radius: 20px;  */
-box-shadow: 10px 14px 17px -8px rgba(0,0,0,0.66);
+  border-radius: 30px;
+  /* box-shadow: 10px 14px 17px -8px rgba(0,0,0,0.66);
 -webkit-box-shadow: 10px 14px 17px -8px rgba(0,0,0,0.66);
--moz-box-shadow: 10px 14px 17px -8px rgba(0,0,0,0.66);
+-moz-box-shadow: 10px 14px 17px -8px rgba(0,0,0,0.66); */
+  font-family: "Poppins", sans-serif;
 }
 
 .community-div p,
@@ -225,13 +382,18 @@ box-shadow: 10px 14px 17px -8px rgba(0,0,0,0.66);
   font-family: "Roboto", sans-serif;
 }
 .growth-div {
-  background: rgb(112, 236, 133);
+  background: #88cc94;
 }
 .you-matter-div {
-  background: rgb(217, 255, 0);
+  background: #ffbc04;
 }
 .calender-div {
-  background: rgb(67, 147, 252);
+  background: #3b8fff;
+}
+
+/* ___CEO */
+.ceo img {
+  border-radius: 50%;
 }
 
 /* __________MATTERS DIV END */
@@ -241,9 +403,9 @@ box-shadow: 10px 14px 17px -8px rgba(0,0,0,0.66);
 #IO {
   font-weight: 900;
 }
-.heading {
+/* .heading {
   font-weight: bolder;
   font-size: 2rem;
-}
+} */
 /* ______CELEBRATION OF TECH END */
 </style>
